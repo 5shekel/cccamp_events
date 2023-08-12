8cca634f6024b93c0ceec5458e39bcfcceb81800
user <yair99@gmail.com>
2023-08-12 18:10:12 +0300

    works with in-progress

--- app.py
+++ app.py
@@ -1,58 +1,98 @@
 import streamlit as st
-from datetime import datetime, timedelta, timezone
-
-current_time = datetime.now(tz=timezone.utc) + timedelta(hours=2)
-
+from datetime import datetime, timedelta
+import pytz
 from parser_backend import get_talk_details_by_text
 import json
 
+neon_color = "#FF1493"
+
+@st.cache_data
+def load_data(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
 def main():
-    st.title('Conference Talks')
+    current_time = datetime.now(pytz.utc) + timedelta(hours=2)
+
+    # //fake current timed
+    current_time = datetime.strptime("2023-08-15T18:00:00+02:00", "%Y-%m-%dT%H:%M:%S%z") + timedelta(hours=2)
+
+    st.title("Camp Talks")
+    #subheader with current time
+    st.markdown(f"<h2 style='font-weight: bold; font-size: 150%;'>Current Time: {current_time.strftime('%A %m %d %I:%M %p')}</h2>", unsafe_allow_html=True)
 
     # Load data from JSON file
-    with open('cccamp.json', 'r') as file:
-        data = json.load(file)
+    data = load_data("cccamp.json")
 
     # User input for text search
-    text_to_search = st.text_input("Search for talks by title, abstract, or speakers:")
+    text_to_search = st.text_input("Search for talks by title, abstract, track or speakers:")
 
     # Get matching talks
-    matching_talks = get_talk_details_by_text(data, text_to_search)
+    matching_talks = get_talk_details_by_text(data, text_to_search, current_time)
 
-    current_time = datetime.now(tz=timezone.utc) + timedelta(hours=2)
 
     in_progress_displayed = False
     upcoming_displayed = False
 
     for idx, talk in enumerate(matching_talks):
-        start_time = datetime.strptime(talk['unmodified_start'], '%Y-%m-%dT%H:%M:%S%z') + timedelta(hours=2)
-        end_time = datetime.strptime(talk['unmodified_end'], '%Y-%m-%dT%H:%M:%S%z') + timedelta(hours=2)
+        title = talk["title"]
+        room = talk["room"]
+
+        # Skip talks with "Lunch" in the title that are not in room 5834
+        if "Lunch" in title and room != "Milliways":
+            continue
+
+        start_time = datetime.strptime(
+            talk["unmodified_start"], "%Y-%m-%dT%H:%M:%S%z"
+        ) + timedelta(hours=2)
+        end_time = datetime.strptime(
+            talk["unmodified_end"], "%Y-%m-%dT%H:%M:%S%z"
+        ) + timedelta(hours=2)
 
         if current_time >= start_time and current_time <= end_time:
             if not in_progress_displayed:
-                st.subheader('In Progress Talks')
+                st.markdown(
+                    "<h1 style='font-size: 200%; color: limegreen;'>in Progress</h1>",
+                    unsafe_allow_html=True,
+                )
                 in_progress_displayed = True
-            st.markdown(f"**{talk['title']}**", unsafe_allow_html=True)
+            st.markdown(
+                f"<h2 style='font-weight: bold; font-size: 150%;'>{title}</h2>",
+                unsafe_allow_html=True,
+            )
         else:
             if not upcoming_displayed:
-                st.subheader('Upcoming Talks')
+                st.markdown(
+                    "<h1 style='font-size: 200%; color: limegreen;'>Upcoming</h1>",
+                    unsafe_allow_html=True,
+                )
                 upcoming_displayed = True
-            st.markdown(f"**{talk['title']}**", unsafe_allow_html=True)
+            st.markdown(
+                f"<h2 style='font-weight: bold; font-size: 150%;'>{title}</h2>",
+                unsafe_allow_html=True,
+            )
 
-        abstract = talk.get('abstract')
+        abstract = talk.get("abstract")
         if abstract:
-            st.write(f"**Abstract:** {abstract}")
+            st.write(f"{abstract}")
 
-        if talk['speakers']:
+        if talk["speakers"]:
             st.write(f"**Speakers:** {', '.join(talk['speakers'])}")
 
-        track = talk.get('track')
+        track = talk.get("track")
+        room = talk["room"]
+
         if track:
-            st.write(f"**Track:** {talk['track']}")
-        st.write(f"**Room:** {talk['room']}")
-        st.write(f"**Start Time:** {talk['start']}")
-        st.write(f"**Duration:** {talk['duration']}")
+            st.write(f"**Track** {talk['track']}")
+        
+        # Only display the room if it's not the same as the track
+        if room != track:
+            st.write(f"**Room:** {room}")
+
+        st.markdown(f"<div style='display: flex; justify-content: space-between;'><div <span style='color: white;'>{talk['start']}</span></div><div>{talk['duration']}</div></div>", unsafe_allow_html=True)
+
         st.write("---")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- parser_backend.py
+++ parser_backend.py
@@ -3,11 +3,10 @@ import pytz
 import argparse
 from datetime import datetime, timedelta
 
-def get_talk_details_by_text(data, text=None):
+def get_talk_details_by_text(data, text=None, current_time=None):
     matching_talks = []
     in_progress_talks = []
     upcoming_talks = []
-    current_time = datetime.now(pytz.utc) + timedelta(hours=2)  # Convert to UTC+2 timezone
 
     for talk in data['talks']:
         title_obj = talk['title']
@@ -21,17 +20,19 @@ def get_talk_details_by_text(data, text=None):
         speaker_names = [speaker['name'] for speaker in data['speakers'] if speaker['code'] in speaker_codes]
         talk['speakers'] = speaker_names
 
+        track_id = talk.get('track', None)
+        if track_id:
+            track_name_obj = next((track['name'] for track in data['tracks'] if track['id'] == track_id), None)
+            track_name = track_name_obj.get('en', track_name_obj.get('de', '')) if track_name_obj else ''
+            talk['track'] = track_name
+
         if text and (text.lower() in title_text.lower() or
-                     text.lower() in abstract_text.lower() or
-                     any(text.lower() in speaker_name.lower() for speaker_name in speaker_names)) or not text:
+                    text.lower() in abstract_text.lower() or
+                    any(text.lower() in speaker_name.lower() for speaker_name in speaker_names) or
+                    (text.lower() in track_name.lower())) or not text:
 
             talk['title'] = title_text  # Set the title to the extracted text
 
-            track_id = talk.get('track', None)
-            if track_id:
-                track_name_obj = next((track['name'] for track in data['tracks'] if track['id'] == track_id), None)
-                track_name = track_name_obj.get('en', track_name_obj.get('de', '')) if track_name_obj else ''
-                talk['track'] = track_name
 
             room_name_obj = next((room['name'] for room in data['rooms'] if room['id'] == talk['room']), None)
             room_name = room_name_obj.get('en', room_name_obj.get('de', '')) if room_name_obj else ''
@@ -39,6 +40,10 @@ def get_talk_details_by_text(data, text=None):
 
             start_time = datetime.strptime(talk['start'], '%Y-%m-%dT%H:%M:%S%z') + timedelta(hours=2)
             end_time = datetime.strptime(talk['end'], '%Y-%m-%dT%H:%M:%S%z') + timedelta(hours=2)
+
+            # Skip past talks
+            if end_time < current_time:
+                continue
 
             talk['unmodified_start'] = talk['start']
             talk['unmodified_end'] = talk['end']
@@ -84,7 +89,7 @@ def main():
     if not text:
         text = input("Please enter the text to search in title, abstract, or speakers: ")
 
-    matching_talks = get_talk_details_by_text(data, text=text)
+    matching_talks = get_talk_details_by_text(data, text=text, current_time=datetime.now(pytz.utc) + timedelta(hours=2))
     print(json.dumps(matching_talks, indent=4))
 
 if __name__ == '__main__':
